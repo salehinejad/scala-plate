0b0cab41647be6ee2e32fd4e7df64a8bbdfacbe5
Salehinejad, Hojjat <hojjat.salehinejad@scotiabank.com>
2016-12-14 09:37:00 -0500

    init

---
+++ code/src/main/scala/Main.scala
@@ -0,0 +1,36 @@
+package io.rbi.tpl8
+
+
+import java.util.Properties
+
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.streams._
+import org.apache.kafka.streams.kstream.{KStream, KStreamBuilder}
+
+object Main {
+  def main(args: Array[String]) {
+
+    println("Stream config starts...");      
+
+    val builder: KStreamBuilder = new KStreamBuilder
+
+    val streamingConfig = {
+      val settings = new Properties
+      settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "map-function-scala-example")
+      settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+      settings.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, "localhost:2181")
+      // Specify default (de)serializers for record keys and for record values.
+      settings.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray.getClass.getName)
+      settings.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
+      settings
+    }
+
+    println("Stream config done.");
+
+
+    val stringSerde: Serde[String] = Serdes.String()
+
+
+
+  }
+}
